d6e8f0ad7f1b672e6c143530df13fe5ab4204b29
Aidan <adn.mck@gmail.com>
2022-08-25 14:21:31 +0100

    added icons, cpu/player round choices

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const buttons = document.querySelectorAll('button');
 const score = document.createElement('p');
 const winner = document.createElement('p');
 const info = document.querySelector('.info');
-const h3 = document.createElement('h3');
 const board = document.querySelector('.board');
 const rules = document.querySelector('.rules');
+const playerChoice = document.querySelector('.player');
+const cpuChoice = document.querySelector('.cpu');
+const pScore = document.querySelector('.pScore');
+const cScore = document.querySelector('.cScore');
+const scoreSeperator = document.querySelector('.lrg');
 
 winner.classList.add("winner");
-score.classList.add("score");
 
 let playerScore = 0;
 let cpuScore = 0;
@@ -18,14 +21,14 @@ for (const button of buttons) {
     button.addEventListener("click", (e) => {
 
         if (playerScore < 5 && cpuScore < 5) {
-            const playerSelection = e.target.textContent;
+            const playerSelection = e.target.classList.value;
             playRound(playerSelection);
         }
     });
 };
 
 function getComputerChoice() {
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["rock", "paper", "scissors"];
     const choice = Math.floor(Math.random() * (options.length - 0) + 0);
     //console.log(`computer choice: ${options[choice]}`);
     return options[choice];
@@ -34,59 +37,59 @@ function getComputerChoice() {
 function playRound(playerSelection) {
 
     const computerSelection = getComputerChoice();
+    displayChoice(playerSelection, computerSelection);
+
     let roundWinner = [];
 
     if (computerSelection === playerSelection) {
-        roundWinner = ["round tied!", "tie"];
-    } else if (computerSelection === "Rock") {
-        if (playerSelection === "Paper") {
-            roundWinner = ["round won! paper beats rock!", "player"];
-        } else if (playerSelection === "Scissors") {
-            roundWinner = ["round lost! rock beats scissors!", "cpu"];
+        roundWinner = ["Tie", computerSelection, playerSelection];
+    } else if (computerSelection === "rock") {
+        if (playerSelection === "paper") {
+            roundWinner = ["Player wins the", computerSelection, playerSelection];
+        } else if (playerSelection === "scissors") {
+            roundWinner = ["Computer wins the", computerSelection, playerSelection];
         }
 
-    } else if (computerSelection === "Paper") {
-        if (playerSelection === "Scissors") {
-            roundWinner = ["round won! scissors beats paper!", "player"];
-        } else if (playerSelection === "Rock") {
-            roundWinner = ["round lost! paper beats rock!", "cpu"];
+    } else if (computerSelection === "paper") {
+        if (playerSelection === "scissors") {
+            roundWinner = ["Player wins the", computerSelection, playerSelection];
+        } else if (playerSelection === "rock") {
+            roundWinner = ["Computer wins the", computerSelection, playerSelection];
         }
 
-    } else if (computerSelection === "Scissors") {
-        if (playerSelection === "Rock") {
-            roundWinner = ["round won! rock beats scissors!", "player"];
-        } else if (playerSelection === "Paper") {
-            roundWinner = ["round lost! scissors beats paper!", "cpu"];
+    } else if (computerSelection === "scissors") {
+        if (playerSelection === "rock") {
+            roundWinner = ["Player wins the", computerSelection, playerSelection];
+        } else if (playerSelection === "paper") {
+            roundWinner = ["Computer wins the", computerSelection, playerSelection];
         }
     }
     
-    if (roundWinner[1] === "cpu") {
+    if (roundWinner[0].startsWith("Computer")) {
         cpuScore++;
-    } else if (roundWinner[1] === "player") {
+        cScore.textContent = cpuScore;
+        pScore.textContent = playerScore;
+    } else if (roundWinner[0].startsWith("Player")) {
         playerScore++;
+        cScore.textContent = cpuScore;
+        pScore.textContent = playerScore;
     }
 
-    if (rules.parentNode) {
-        rules.style.height = "0px";
-        rules.parentNode.removeChild(rules);
-    }
-
-    h3.textContent = "score:";
-    score.textContent = `${playerScore} - ${cpuScore}`;
-    winner.textContent = roundWinner[0];
-    info.appendChild(h3);
-    info.appendChild(score);
+    scoreSeperator.textContent = "-";
+    winner.textContent = `${roundWinner[0]} round!`;
     info.appendChild(winner);
 
     if (cpuScore === 5) {
         winner.classList.add("gameOver");
         winner.textContent = "game over! computer wins!";
-        info.removeChild(score);
-        info.removeChild(h3);
     } else if (playerScore === 5) {
         winner.classList.add("gameOver");
         winner.textContent = "game over! you win!";
-        info.removeChild(score);
-        info.removeChild(h3);
     }
 };
+
+function displayChoice(playerSelection, computerSelection) {
+
+    playerChoice.style.backgroundImage = (`url("/images/${playerSelection}.png")`);
+    cpuChoice.style.backgroundImage = (`url("images/${computerSelection}.png")`);
+}
